eb3a013a18367431fa9b98286d95abbc29fdf786
luobo656 <2623093705@qq.com>
2026-08-27 00:47:29 +0800

    Add assembly metadata

---
+++ lanproxy-wpf-v390/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("局域网代理共享助手")]
+[assembly: AssemblyDescription("将本机 HTTP/SOCKS5 代理安全共享给同一局域网设备")]
+[assembly: AssemblyCompany("LAN Proxy Share Assistant")]
+[assembly: AssemblyProduct("局域网代理共享助手")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: ComVisible(false)]
+[assembly: Guid("B126A9A0-57C4-4980-A4B7-48696521C9A8")]
+[assembly: AssemblyVersion("3.9.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
